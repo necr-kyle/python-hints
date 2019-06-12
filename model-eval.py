31d99ebc8e93602d85d87547161cf7099af73dbe
Kyle <41163396+necr-kyle@users.noreply.github.com>
2019-06-13 00:44:44 +0800

    Create model-eval.py

---
+++ model-eval.py
@@ -0,0 +1,11 @@
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+
+'''
+X, Y = some training data
+model = some model
+Y_hat = model.predict(X)
+f1_s = f1_score(Y, Y_hat)
+'''
