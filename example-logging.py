198830d787c9455a871bec3e92a28b071f4cd35e
Kun Huang <huangkun_kyle@pku.edu.cn>
2020-02-11 18:05:04 +0800

    delete old files

--- example-logging.py
+++
@@ -1,8 +0,0 @@
-import logging
-logger = logging.getLogger(__name__)
-
-logger.info("hello!")
-# There are other message types like:
-# warning, error, critical, log, debug, exception,
-
-logging.basicConfig(filename=args.logging_output, level=logging.DEBUG)
