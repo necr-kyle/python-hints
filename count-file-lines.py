6e56655115725b7e8e156d941c3e7afffc00b213
Kyle <41163396+necr-kyle@users.noreply.github.com>
2020-04-23 13:37:22 +0800

    Create count-file-lines.py

---
+++ count-file-lines.py
@@ -0,0 +1,5 @@
+from itertools import (takewhile,repeat)
+def rawincount(filename):
+    f = open(filename, 'rb')
+    bufgen = takewhile(lambda x: x, (f.raw.read(1024*1024) for _ in repeat(None)))
+    return sum( buf.count(b'\n') for buf in bufgen )
