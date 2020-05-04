990518346a31789509e15f740749e2e6c92b0f21
Kyle <41163396+necr-kyle@users.noreply.github.com>
2020-05-04 13:38:55 +0800

    Update io-path.py

--- standard-library/io-path.py
+++ standard-library/io-path.py
@@ -1,10 +1,10 @@
 # Go through all files under ./directory
 import os
-directory='./directory'
-start=None
-end=None
-for file in list(os.listdir(directory))[start:end]:
-    route=os.path.join(directory,file)
+directory = './directory'
+start = None
+end = None
+for filename in list(os.listdir(directory))[start:end]:
+    route = os.path.join(directory, filename)
     if file.endswith(".txt"): 
         print(file)
     elif os.path.isdir(route):
