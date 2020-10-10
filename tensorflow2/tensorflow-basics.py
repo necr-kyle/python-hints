46f68f49d9fcd6ae31457a7f774de9d7c3635c03
Kyle <41163396+necr-kyle@users.noreply.github.com>
2020-10-10 12:35:48 +0800

    Update tensorflow-basics.py

--- tensorflow2/tensorflow-basics.py
+++ tensorflow2/tensorflow-basics.py
@@ -5,3 +5,13 @@ if __name__ == "__main__":
     # The "tensor" will be used as an abbreviation of tensorflow.python.framework.ops.EagerTensor
     # tensor.numpy() turns a 0-d tensor to Python built-in type
     print(a[0].numpy())
+    a = tf.convert_to_tensor([[3],[6],[9]])
+    print(a.shape, a)
+    
+    a = tf.squeeze(a, axis=1)
+    print(a.shape, a)
+    a = tf.expand_dims(a, axis=0)
+    print(a.shape, a)
+    
+    a = tf.concat((a,a), axis=0)
+    print(a.shape, a)
